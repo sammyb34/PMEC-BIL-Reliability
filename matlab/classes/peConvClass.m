0bee688e441865e4d1ede9c59db1ec986b7607d8
bartonsa <56001625+sammyb34@users.noreply.github.com>
2024-10-04 19:58:28 -0700

    Changed the class folders
    both the peConv and xLine classes.

--- matlab/classes/peConvClass.m
+++ matlab/classes/peConvClass.m
@@ -1,10 +1,18 @@
-classdef peConvClass
+%% Class Definition: peConvClass
+% peConvClass creates a 'conv' object that holds is used to set the
+% electrical parameters of the power electronic converter (number of
+% devices, device on-resistance(s), switching energy losses, etc. 
+% Also holds functions for calculating averaged switching losses, etc. 
+classdef peConvClass<handle
     properties 
-        type {mustBeText} = "undefined";
-        nDev {mustBeNumeric} = 999;
-    end
+        type    {mustBeText}        = "undefined";
+        num_dev {mustBeNumeric}     = 999;
+        rdson   {mustBeNumeric}     = 999;
+        vf      {mustBeNumeric}     = 0; 
+        rdio    {mustBeNumeric}     = 999;
+    end 
     methods
-        function obj = peConvClass(type, nDev)
+        function obj = peConvClass(type, num_dev, rdson, vf, rdio)
             % Constructing an object from the peConvClass class, not really
             % sure how this works or whether it's worth it but I'm going
             % for it anyways. 
@@ -12,12 +20,28 @@ classdef peConvClass
             if(isstring(type))
                 obj.type = type;
             else
-                error('type is not a string')
+                error("'Type' is not a string")
             end
-
-            obj.nDev = nDev;
+            obj.num_dev = num_dev;
+            obj.rdson = rdson; 
+            obj.vf = vf; 
+            obj.rdio = rdio; 
         end
 
+        function obj = update_rdson(obj, junction_temp)
+            % Considering the variance in rdson with device junction
+            % temperature. This is just a place holder, and should work on
+            % this on Monday. 
+            scale = (junction_temp - 50) / 100; 
+            obj.rdson = obj.rdson * scale;
 
+        end
+
+        function p_cond = getConductionLoss(obj, i_rms)
+            % Calculates conduction losses for a single device.
+            idev_rms = i_rms / sqrt(2); 
+            p_cond = idev_rms^2 * obj.rdson;
+        end
+        
     end
 end
