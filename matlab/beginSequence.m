d441b2d0eddfa8ef3b4087d28bd43e705cd6f39d
bartonsa <56001625+sammyb34@users.noreply.github.com>
2024-12-23 08:59:14 -0800

    Modfied beginSequence
    Made an auto-check so WEC-Sim source not defined multiple times.
    
    Also made it so all subfolders in PMEC-BIL-Reliability directory added to path as well.

--- matlab/beginSequence.m
+++ matlab/beginSequence.m
@@ -3,28 +3,34 @@
 % necessary functions and models
 clear; clc; close all; 
 
-home_path = pwd; 
-wecsim_path = "C:\Users\bartonsa\WEC-Sim\"; % Must change this for each location
-cd(wecsim_path)
-addWecSimSource
-cd(home_path)
+s       = pathsep;
+% wecsim_path = "C:\Users\bartonsa\WEC-Sim\"; % Must change this for each location
+wecsim_path = "C:\Users\smbar\WEC-Sim\source";
+pathStr = [s, path, s];
+onPath  = contains(pathStr, [s, wecsim_path, s], 'IgnoreCase', ispc);
+if onPath == 0
+    home_path = pwd; 
+    cd(wecsim_path)
+    addWecSimSource
+    cd(home_path)
+end
 
 classes_path = fullfile(pwd,'classes');
-addpath(classes_path);
+addpath(genpath(classes_path));
 
 models_path = fullfile(pwd,'models');
-addpath(models_path);
+addpath(genpath(models_path));
 
 utils_path = fullfile(pwd,'utils'); 
-addpath(utils_path);
+addpath(genpath(utils_path));
 
 results_path = fullfile(pwd,'results'); 
-addpath(results_path);
+addpath(genpath(results_path));
 
 lib_path = fullfile(pwd,'lib'); 
-addpath(lib_path);
+addpath(genpath(lib_path));
 
 tests_path = fullfile(pwd,'tests');
-addpath(tests_path)
+addpath(genpath(tests_path));
 
 clear 
